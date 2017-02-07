758586a887c2d501b0453818273dba6c3dba4a32
Hajaniaina <francisco.apsesame2013@gmail.com>
2017-02-08 00:23:09 +0300

    mety

--- src/main/java/utilitaire/UtilDB.java
+++ src/main/java/utilitaire/UtilDB.java
@@ -1,20 +1,17 @@
 package utilitaire;
 
+import java.net.URI;
 import java.sql.*;
 
 public class UtilDB
 {  
     public Connection getConn() throws Exception
     {
-        Class.forName("org.postgresql.Driver");
-        System.out.print("odddk");
-        String url = "jdbc:postgresql://localhost:5432/sakafookoo";
-        
-        String user = "postgres";
-        String passwd = "niaina";
+       URI dbUri = new URI(System.getenv("postgres://fadibelbgvgquh:547e4b2004019c31bdc29db917fe4648447cd4b4e4efd147df661ab9be884238@ec2-54-163-253-94.compute-1.amazonaws.com:5432/ddjmtri86ldgpl"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-        Connection conn = DriverManager.getConnection(url, user, passwd);
-        
-        return conn;
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
