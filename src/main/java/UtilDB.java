f31f209da5141e50196a614ef32f3ddcebce06fa
Hajaniaina <francisco.apsesame2013@gmail.com>
2017-02-07 23:55:23 +0300

    r

--- src/main/java/UtilDB.java
+++ src/main/java/UtilDB.java
@@ -1,13 +1,19 @@
 
 
+
+
 import java.net.URI;
 import java.sql.*;
 
 public class UtilDB
 {  
-    public Connection getConn()  throws URISyntaxException, SQLException
+    public Connection getConn() throws Exception
     {
-       String dbUrl = System.getenv("postgres://fadibelbgvgquh:547e4b2004019c31bdc29db917fe4648447cd4b4e4efd147df661ab9be884238@ec2-54-163-253-94.compute-1.amazonaws.com:5432/ddjmtri86ldgpl");
-       return DriverManager.getConnection(dbUrl);
+       URI dbUri = new URI(System.getenv("postgres://fadibelbgvgquh:547e4b2004019c31bdc29db917fe4648447cd4b4e4efd147df661ab9be884238@ec2-54-163-253-94.compute-1.amazonaws.com:5432/ddjmtri86ldgpl"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
