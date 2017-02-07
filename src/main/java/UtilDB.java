c03d3d1f33b665c92e0c569cfdeafbbf0a9656ba
Hajaniaina <francisco.apsesame2013@gmail.com>
2017-02-07 23:41:12 +0300

    first commit util db

--- src/main/java/UtilDB.java
+++ src/main/java/UtilDB.java
@@ -1,18 +1,17 @@
 
 
+import java.net.URI;
 import java.sql.*;
 
 public class UtilDB
 {  
     public Connection getConn() throws Exception
     {
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/sakafookoo";
-        String user = "postgres";
-        String passwd = "niaina";
+       URI dbUri = new URI(System.getenv("postgres://wbrueegggklfdg:f5ee4be198cb238354f92c461f6b98b7eb45e88adeb2ad3020836cb8973c352d@ec2-54-235-245-255.compute-1.amazonaws.com:5432/d7i51gj4rvqe99"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-        Connection conn = DriverManager.getConnection(url, user, passwd);
-       // System.out.print("odddk");
-        return conn;
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
