370ad27578642a9a9754d18852ce6df648e07509
Hajaniaina <francisco.apsesame2013@gmail.com>
2017-02-08 01:15:20 +0300

    d

--- src/main/java/utilitaire/UtilDB.java
+++ src/main/java/utilitaire/UtilDB.java
@@ -12,7 +12,7 @@ public class UtilDB
          String password ="";
           String dbUrl ="";
       try{
-       URI dbUri = new URI(System.getenv("postgres://fadibelbgvgquh:547e4b2004019c31bdc29db917fe4648447cd4b4e4efd147df661ab9be884238@ec2-54-163-253-94.compute-1.amazonaws.com:5432/ddjmtri86ldgpl"));
+       URI dbUri = new URI(System.getenv("DATABASE_URL"));
          username = dbUri.getUserInfo().split(":")[0];
          password = dbUri.getUserInfo().split(":")[1];
          dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
