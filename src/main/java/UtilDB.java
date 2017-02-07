73e436a1d81e2a7a8ab8fbe239782ac296daf646
Hajaniaina <francisco.apsesame2013@gmail.com>
2017-02-07 23:49:06 +0300

    ttt

--- src/main/java/UtilDB.java
+++ src/main/java/UtilDB.java
@@ -5,13 +5,9 @@ import java.sql.*;
 
 public class UtilDB
 {  
-    public Connection getConn() throws Exception
+    public Connection getConn()  throws URISyntaxException, SQLException
     {
-       URI dbUri = new URI(System.getenv("postgres://wbrueegggklfdg:f5ee4be198cb238354f92c461f6b98b7eb45e88adeb2ad3020836cb8973c352d@ec2-54-235-245-255.compute-1.amazonaws.com:5432/d7i51gj4rvqe99"));
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
+       String dbUrl = System.getenv("postgres://wbrueegggklfdg:f5ee4be198cb238354f92c461f6b98b7eb45e88adeb2ad3020836cb8973c352d@ec2-54-235-245-255.compute-1.amazonaws.com:5432/d7i51gj4rvqe99");
+       return DriverManager.getConnection(dbUrl);
     }
 }
